8b796fa0701f65c82ac326186c03de0493023f1b
Reborn <ren_xiaoyao@outlook.com>
2019-06-18 11:54:22 +0800

    init springboot+thymeleaf+mybatis project

---
+++ src/main/java/com/mallotec/reb/corporatetraining/CorporateTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.mallotec.reb.corporatetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateTrainingApplication.class, args);
+    }
+
+}
